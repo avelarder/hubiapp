fb1a20b8a40b1821a8edf8b493dda28240c3d182
avelarder <velarde.rojas.arturo@gmail.com>
2021-08-08 22:16:20 -0300

    Fixing activationmodal component

--- pages/register/activate.js
+++ pages/register/activate.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import ActivationModal from "./activationModal";
+import ActivationModal from "../../components/activationModal";
 
 function Activate() {
   const router = useRouter();
