a4ba47910b5ecf1feba99018464025b290d175cb
avelarder <velarde.rojas.arturo@gmail.com>
2022-03-06 22:39:50 -0300

    Partial changes

--- components/common/field-container.js
+++ components/common/field-container.js
@@ -1,11 +1,14 @@
-
 function FieldContainer({ title, children }) {
-    return (<div className="mt-2 border-1 border-gray-50 rounded-xl p-2">
-        <span className="block text-xs font-semibold text-gray-400">
-            {title}
+  return (
+    <div className="mt-2 border-0 border-gray-50 rounded-xl p-2">
+      {title && (
+        <span className="ml-4 block text-xs font-semibold text-gray-400">
+          {title}
         </span>
-        {children}
-    </div>)
+      )}
+      {children}
+    </div>
+  );
 }
 
 export default FieldContainer;
