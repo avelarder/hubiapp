e815c44996f43d84fe7d3281f08b00f3ec9b3a3c
avelarder <velarde.rojas.arturo@gmail.com>
2022-05-22 20:17:09 -0300

    Aligment

--- components/dashboard/notification.jsx
+++ components/dashboard/notification.jsx
@@ -4,7 +4,7 @@ import { BellIcon, QuestionMarkCircleIcon } from "@heroicons/react/solid";
 
 function Notification() {
   return (
-    <div className="flex justify-start">
+    <div className="flex justify-start items-end">
       <Search></Search>
       <BellIcon className="text-purple-500 h-5 w-5 m-2"></BellIcon>
       <QuestionMarkCircleIcon className="text-purple-500 h-5 w-5 m-2"></QuestionMarkCircleIcon>
--- components/dashboard/search.jsx
+++ components/dashboard/search.jsx
@@ -5,7 +5,7 @@ function Search() {
   return (
     <div className="flex">
       <div className="relative flex">
-        <input className="lg:max-w-full rounded-xl border-1 border-purple-500"></input>
+        <input className="h-10 0lg:max-w-full rounded-full border-1 border-purple-300"></input>
         <span className="absolute inset-y-0 right-0 flex items-center">
           <SearchIcon className="relative text-purple-500 h-5 w-5 m-2"></SearchIcon>
         </span>
