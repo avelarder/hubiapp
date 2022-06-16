b02081cbd6f408b93fcf62f108e68f78c62dc624
arturo.velarde.gitlab <arturo.velarde+gitlab@kzsoftworks.com>
2022-06-16 20:53:20 -0300

    adding social

---
+++ pages/social/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "../../components/layout";
+import MainSection from "../../components/dashboard/mainSection";
+
+function SocialHomePage() {
+  return (
+    <Layout>
+      <div className="px-4 sm:px-6 lg:px-8 py-8 mx-auto">
+        <MainSection></MainSection>
+      </div>
+    </Layout>
+  );
+}
+
+export default SocialHomePage;
