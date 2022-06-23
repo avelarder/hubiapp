183fabf95dad49b46bc3cdbf2939bf68b5e3c405
arturo.velarde.gitlab <arturo.velarde+gitlab@kzsoftworks.com>
2022-06-22 23:33:03 -0300

    Refactoring Posts

--- components/common/textInput.jsx
+++ components/common/textInput.jsx
@@ -4,68 +4,62 @@ import InputMask from "react-input-mask";
 import TextareaAutosize from "react-textarea-autosize";
 
 const TextInput = ({
+  value,
+  onChange,
+  mask,
+  validation,
+  invalidText,
 
-    value,
-    onChange,
-    mask,
-    validation,
-    invalidText,
-
-    ...props
+  ...props
 }) => {
-    const [validInput, setValidInput] = useState(true);
+  const [validInput, setValidInput] = useState(true);
 
-    const inputClassNames = classnames(
+  //  className={classNames(
+  //     "bg-gradient-to-r from-gray-50 to-white text-xs text-gray-500  w-full h-10 border-grayt-200 rounded-full border-b-1 focus:border-purple-900 pl-6",
+  //     { "text-black bg-red-200": hasError }
+  //   )}
+  const inputClassNames = classnames(
+    "bg-gradient-to-r from-gray-50 to-white text-xs text-gray-500  w-full h-10 border-grayt-200 rounded-full border-b-1 focus:border-purple-900 pl-6",
+    { "border-red-500": !validInput }
+  );
 
-        "text-sm text-gray-500 w-40 h-10 border-gray-200 rounded-lg p-2 border-1",
-        { "border-red-500": !validInput, }
-    );
-
-    return (
-        <div className="relative w-full">
-
-            {mask ? (
-                <InputMask
-                    className={inputClassNames}
-                    value={value}
-                    onChange={(e) => {
-                        onChange(e.target.value);
-                    }}
-                    onBlur={(e) => {
-                        if (validation) {
-                            setValidInput(validation(e.target.value));
-                        }
-                    }}
-                    mask={mask}
-                    {...props}
-                ></InputMask>
-            ) : (
-                <TextareaAutosize
-                    value={
-                        value
-                    }
-                    onChange={(e) => {
-                        onChange(e.target.value);
-                    }}
-                    onBlur={(e) => {
-                        if (validation) {
-                            const validationResult = validation(e.target.value);
-                            setValidInput(validationResult);
-
-                        }
-                    }}
-                    {...props}
-                ></TextareaAutosize>
-
-            )
+  return (
+    <div className="relative w-full">
+      {mask ? (
+        <InputMask
+          className={inputClassNames}
+          value={value}
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+          onBlur={(e) => {
+            if (validation) {
+              setValidInput(validation(e.target.value));
             }
-            {
-                !validInput && invalidText && (
-                    <p className="text-red-500 text-xs italic">{invalidText}</p>
-                )
+          }}
+          mask={mask}
+          {...props}
+        ></InputMask>
+      ) : (
+        <TextareaAutosize
+          value={value}
+          onChange={(e) => {
+            onChange(e.target.value);
+          }}
+          onBlur={(e) => {
+            if (validation) {
+              const validationResult = validation(e.target.value);
+              setValidInput(validationResult);
             }
-        </div >
-    );
+          }}
+          {...props}
+        ></TextareaAutosize>
+      )}
+      {!validInput && invalidText && (
+        <p className="text-red-500 text-xs italic">{invalidText}</p>
+      )}
+    </div>
+  );
 };
 
 export default TextInput;
