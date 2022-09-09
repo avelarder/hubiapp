8fc7e7a17343db7a01f0cd4dd9b9c96de2a6d674
avelarder <velarde.rojas.arturo@gmail.com>
2022-09-09 00:23:42 -0300

    Access

--- pages/app/configuracion/equipo/accesos.jsx
+++ pages/app/configuracion/equipo/accesos.jsx
@@ -8,13 +8,14 @@ import { useLocationContext } from "../../../../locationProvider";
 import Firebase from "../../../../firebase";
 import useFirestoreQuery from "../../../../hooks/useFirestoreQuery";
 
-function AccessToggleSection({
-  title,
-  description,
-  toogleState,
-  onToggle,
-  options,
-}) {
+function AccessToggleSection({ title, description, toggleState, onToggle }) {
+  const [toggle, setToggle] = useState(toggleState);
+
+  const handleToggle = (value) => {
+    setToggle(value);
+    onToggle(value);
+  };
+
   return (
     <FieldContainer>
       <div className="flex flex-col  border-1 border-gray-400 my-4 rounded-lg ">
@@ -25,77 +26,93 @@ function AccessToggleSection({
           </div>
           <div className="w-1/4 justify-end items-end">
             <Switch
-              checked={toogleState}
-              onChange={onToggle}
-              className={`${toogleState ? "bg-purple-900" : "bg-purple-200"}
+              checked={toggle}
+              onChange={handleToggle}
+              className={`${toggle ? "bg-purple-900" : "bg-purple-200"}
           relative inline-flex flex-shrink-0 h-5 w-10 border-1 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
             >
               <span
                 aria-hidden="true"
-                className={`${toogleState ? "translate-x-5" : "translate-x-0"}
+                className={`${toggle ? "translate-x-5" : "translate-x-0"}
             pointer-events-none inline-block h-4 w-4 rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
               />
             </Switch>
           </div>
         </div>
-        {toogleState && (
-          <FieldContainer>
-            <div className="flex flex-col items-start border-1 border-gray-300 m-4 p-4 rounded-lg bg-gray-50 text-sm ">
-              {options &&
-                options.map((option, index) => (
-                  <div key={index} className="flex gap-2 items-center px-4">
-                    <input
-                      type={"checkbox"}
-                      onChange={option.onChange}
-                      value={option.value}
-                    />
-                    <span>{option.title}</span>
-                  </div>
-                ))}
-            </div>
-          </FieldContainer>
-        )}
       </div>
     </FieldContainer>
   );
 }
 
+function AccessOptionsSection({ options }) {
+  return (
+    <div className="flex flex-col  border-1 border-gray-400 my-4 rounded-lg ">
+      <FieldContainer>
+        <div className="flex flex-col items-start border-1 border-gray-300 m-4 p-4 rounded-lg bg-gray-50 text-sm ">
+          {options &&
+            options.map((option, index) => (
+              <div key={index} className="flex gap-2 items-center px-4">
+                <input
+                  type={"checkbox"}
+                  onChange={option.onChange}
+                  value={option.value}
+                />
+                <span>{option.title}</span>
+              </div>
+            ))}
+        </div>
+      </FieldContainer>
+    </div>
+  );
+}
+
 function AccessPage() {
   const db = Firebase.default.firestore();
-  const {
-    locationSelected,
-    availableLocations,
-    setSelectedLocation,
-    setAvailableLocations,
-  } = useLocationContext();
+  const { locationSelected } = useLocationContext();
 
   const [collaboratorType, setCollaboratorType] = useState(
     collaboratorTypeOptions[0]
   );
-  const [movileAccessEnabled, setMovileAccessEnabled] = useState(false);
-  const [desktopAccessEnabled, setDesktopAccessEnabled] = useState(false);
-  const [dashboardAccessEnabled, setDashboardAccessEnabled] = useState(false);
+  // const [mobileAccessEnabled, setMobileAccessEnabled] = useState(false);
+  // const [desktopAccessEnabled, setDesktopAccessEnabled] = useState(false);
+  // const [dashboardAccessEnabled, setDashboardAccessEnabled] = useState(false);
 
-  const query = db
+  const [dashboardOptions, setDashboardOptions] = useState([]);
+
+  const queryProfiles = db
     .collection("AccessProfiles")
     .where("locationId", "==", locationSelected.id ?? "");
 
-  const { data, status, error } = useFirestoreQuery(query);
+  const {
+    data: dataProfiles,
+    status: statusProfiles,
+    error,
+  } = useFirestoreQuery(queryProfiles);
 
   useEffect(() => {
-    if (data) {
-      setMovileAccessEnabled(data[0]?.movileAccess);
-      setDesktopAccessEnabled(data[0]?.desktopAccess);
-      setDashboardAccessEnabled(data[0]?.dashboardAccess);
-    }
-  }, [
-    data,
-    setMovileAccessEnabled,
-    setDesktopAccessEnabled,
-    setDashboardAccessEnabled,
-  ]);
+    if (dataProfiles) {
+      const profile = dataProfiles.find((x) => x.role === collaboratorType.id);
 
-  if (status === "loading") {
+      for (let index = 0; index < profile?.options.length; index++) {
+        const element = profile.options[index];
+
+        element.onSnapshot((response) => {
+          const option =
+            response.exists === true
+              ? { id: response.id, ...response.data() }
+              : null;
+
+          setDashboardOptions((prev) => [...prev, option]);
+        });
+      }
+    }
+  }, [dataProfiles, collaboratorType]);
+
+  const handleMobileAccess = (value) => {};
+  const handleDashboardAccess = (value) => {};
+  const handleDesktopdAccess = (value) => {};
+
+  if (statusProfiles === "loading") {
     return (
       <div className="flex justify-center items-center w-full bg-purple-100 h-10 text-xs text-purple-500">
         Los datos se están cargando, un momento por favor.
@@ -103,7 +120,7 @@ function AccessPage() {
     );
   }
 
-  if (status === "error") {
+  if (statusProfiles === "error") {
     return `Error: ${error.message}`;
   }
 
@@ -127,73 +144,53 @@ function AccessPage() {
             </div>
           </FieldContainer>
         </div>
-        {data?.length > 0 && data[0].role == collaboratorType.id && (
-          <div className="flex flex-col justify-center mt-8">
-            <span className="font-bold text-2xl">Permisos</span>
-            <AccessToggleSection
-              title={"Acceso a Hubi desde el celular"}
-              description={
-                "Permite a los empleados tengan acceso con su password de 4 dígitos desde su móvil."
-              }
-              onToggle={setMovileAccessEnabled}
-              key={"movileAccess"}
-              toogleState={movileAccessEnabled}
-              options={[
-                {
-                  title: "Option 1",
-                  value: false,
-                  onChange: () => {},
-                },
-                {
-                  title: "Option 2",
-                  value: false,
-                  onChange: () => {},
-                },
-                {
-                  title: "Option 3",
-                  value: false,
-                  onChange: () => {},
-                },
-              ]}
-            ></AccessToggleSection>
-            <AccessToggleSection
-              title={"Acceso a Hubi desde el escritorio (desktop)"}
-              description={
-                " Permite a los empleados tengan acceso con su password de 4 dígitos desde su computador."
-              }
-              onToggle={setDesktopAccessEnabled}
-              key={"desktopAccess"}
-              toogleState={desktopAccessEnabled}
-              options={[
-                {
-                  title: "Option 1",
-                  value: false,
-                  onChange: () => {},
-                },
-                {
-                  title: "Option 2",
-                  value: false,
-                  onChange: () => {},
-                },
-                {
-                  title: "Option 3",
-                  value: false,
-                  onChange: () => {},
-                },
-              ]}
-            ></AccessToggleSection>
-            <AccessToggleSection
-              title={"Acceso al Dashboard"}
-              description={
-                "Permite a los empleados tengan acceso con su password de 4 dígitos desde su computador."
-              }
-              onToggle={setDashboardAccessEnabled}
-              key={"dashboardAccess"}
-              toogleState={dashboardAccessEnabled}
-              options={[]}
-            ></AccessToggleSection>
-          </div>
-        )}
+        {dataProfiles?.length > 0 &&
+          dataProfiles.find((x) => x.role == collaboratorType.id) && (
+            <div className="flex flex-col justify-center mt-8">
+              <span className="font-bold text-2xl">Permisos</span>
+              <AccessToggleSection
+                title={"Acceso a Hubi desde el celular"}
+                description={
+                  "Permite a los empleados tengan acceso con su password de 4 dígitos desde su móvil."
+                }
+                onToggle={handleMobileAccess}
+                key={"movileAccess"}
+                toggleState={
+                  dataProfiles.find((x) => x.role == collaboratorType.id)
+                    .mobileAccess
+                }
+              ></AccessToggleSection>
+              <AccessToggleSection
+                title={"Acceso a Hubi desde el escritorio (desktop)"}
+                description={
+                  " Permite a los empleados tengan acceso con su password de 4 dígitos desde su computador."
+                }
+                onToggle={handleDesktopdAccess}
+                key={"desktopAccess"}
+                toggleState={
+                  dataProfiles.find((x) => x.role == collaboratorType.id)
+                    .desktopAccess
+                }
+              ></AccessToggleSection>
+              <AccessToggleSection
+                title={"Acceso al dashboard"}
+                description={
+                  " Permite a los empleados tengan acceso con su password de 4 dígitos desde su computador."
+                }
+                onToggle={handleDashboardAccess}
+                key={"dashboardAccess"}
+                toggleState={
+                  dataProfiles.find((x) => x.role == collaboratorType.id)
+                    .dashboardAccess
+                }
+              ></AccessToggleSection>
+              <AccessOptionsSection
+                options={dashboardOptions.map((x) => {
+                  return { title: x.title, value: x.enabled };
+                })}
+              ></AccessOptionsSection>
+            </div>
+          )}
       </div>
     </NewLayout>
   );
